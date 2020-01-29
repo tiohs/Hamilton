02a9fc574b5060db15e28674f9ad5d6575791da3
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-01-29 10:04:43 +0100

    Fase de teste

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+msg = new Array();
+msg[0] = "<p>Olá, Eu sou Hamilton Silva !</p>";
+
+text1 = "";
+text2 = "";
+count = 0;
+count2 = 0;
+text = msg[0].split("");
+
+function writetext() {
+  text1 = text2 + " <span color='silver'> " + text[count] + " </span> ";
+  text2 += text[count];
+  var $apresent = document.querySelector('#apresent');
+  $apresent.innerHTML = text1;
+  if (count < text.length - 1) {
+    count++;
+    setTimeout('writetext()', 50);
+  } else {
+    count = 0;
+    if (count2 != 4) {
+      count2++;
+      text2 += " ";
+      text = eval('msg[' + count2 + '].split("")');
+      setTimeout('writetext()', 50);
+    }
+  }
+}
