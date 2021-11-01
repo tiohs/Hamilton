388a5e949e1bba4137e6b24c1163b4eeb8c82d20
tiohs <hamiltonsilva551@gmail.com>
2021-11-01 11:32:31 +0100

    scroll pronto para subir

--- public/js/main.js
+++ public/js/main.js
@@ -42,7 +42,7 @@ const scrollReveal = ScrollReveal({
   distance: '30px',
   duration: 700,
   reset: true
-})
+});
 
 scrollReveal.reveal(
   ` section#redesSocias ul li,
@@ -50,4 +50,28 @@ scrollReveal.reveal(
     section#about .info .conct
   `,
   { interval: 100 }
-)
+);
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
